8eb6addcc51f1765f87f5537f506fc5fd2cab984
Toby Lee <tobyilee@gmail.com>
2022-09-03 09:34:22 +1000

    1.2.3 DB 커넥션 만들기의 독립

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,69 +8,63 @@ import java.sql.SQLException;
 
 import springbook.user.domain.User;
 
-public class UserDao {
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+public abstract class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
 
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+		PreparedStatement ps = c.prepareStatement(
+			"insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
 
-        ps.executeUpdate();
+		ps.executeUpdate();
 
-        ps.close();
-        c.close();
-    }
-
-
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
-        PreparedStatement ps = c
-                .prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return user;
-    }
+		ps.close();
+		c.close();
+	}
 
 
-    private Connection getConnection() throws ClassNotFoundException,
-            SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
-                "book");
-        return c;
-    }
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
+		PreparedStatement ps = c
+				.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new UserDao();
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
 
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+		rs.close();
+		ps.close();
+		c.close();
 
-        dao.add(user);
+		return user;
+	}
 
-        System.out.println(user.getId() + " 등록 성공");
+	abstract protected Connection getConnection() throws ClassNotFoundException, SQLException ;
 
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
-    }
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new NUserDao();
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+			
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+			
+		System.out.println(user2.getId() + " 조회 성공");
+	}
 
 }
