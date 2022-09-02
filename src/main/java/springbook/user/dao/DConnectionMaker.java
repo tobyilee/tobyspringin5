8e0786ae0fcff2fe80ff08442cc32b16f92bbd76
Toby Lee <tobyilee@gmail.com>
2022-09-03 09:46:08 +1000

    1.4.2 오브젝트 팩토리의 활용 (driver class 수정)

--- src/main/java/springbook/user/dao/DConnectionMaker.java
+++ src/main/java/springbook/user/dao/DConnectionMaker.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DConnectionMaker implements ConnectionMaker {
 	public Connection makeConnection() throws ClassNotFoundException,
 			SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection c = DriverManager.getConnection(
 				"jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring", "book");
 		return c;
