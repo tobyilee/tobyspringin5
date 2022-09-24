842c03acf9cf49a86d387847947c6f5a64e72d64
Toby Lee <tobyilee@gmail.com>
2022-09-25 06:25:07 +1000

    2.3.3

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -1,8 +1,12 @@
 package springbook.user.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String id;
     private String name;
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -30,4 +30,26 @@ public class UserDaoTest {
 		assertThat(user2.getName()).isEqualTo(user.getName());
 		assertThat(user2.getPassword()).isEqualTo(user.getPassword());
 	}
+
+	@Test
+	public void count() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("gyumee", "박성철", "springno1");
+		User user2 = new User("leegw700", "이길원", "springno2");
+		User user3 = new User("bumjin", "박범진", "springno3");
+
+		dao.deleteAll();
+		assertThat(dao.getCount()).isEqualTo(0);
+
+		dao.add(user1);
+		assertThat(dao.getCount()).isEqualTo(1);
+
+		dao.add(user2);
+		assertThat(dao.getCount()).isEqualTo(2);
+
+		dao.add(user3);
+		assertThat(dao.getCount()).isEqualTo(3);
+	}
 }
